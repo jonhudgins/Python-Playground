e37f1cbfc7f1d4154bc1974839a58a76d954f0f0
Jonathan Hudgins <Jhudgins@hks.harvard.edu>
2020-01-13 17:20:28 -0500

    print superhero powers to terminal

---
+++ superheros.py
@@ -0,0 +1,22 @@
+import json
+
+with open('superheroes.json', 'r') as f:
+    superheroes = json.load(f)
+    
+# print(superheroes)
+
+
+# creates an empty array called powers 
+powers = []
+
+# Loop through the members of the squad and 
+
+# append the powers of each to the powers array 
+
+members = superheroes['members']
+for member in members:
+	this_members_powers = member['powers']
+	powers.append(this_members_powers)
+
+# prints those powers to the terminal 
+print(powers)
